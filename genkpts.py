9faae032c52388aadc26a30387b736a846f2ecfe
Dongyuan-Ni <45381254+Dongyuan-Ni@users.noreply.github.com>
2022-04-28 22:12:51 +0800

    Update genkpts.py

--- genkpts.py
+++ genkpts.py
@@ -1,12 +1,15 @@
 import os
 import numpy as np
 from numpy.linalg import *
-# x->-0.1 to 0.1
+# x->-0.2 to 0.2
 # y->0
-# z->0.8, 1
+# z->-0.2 to 0.2
 kpts = []
-for i in range(-20, 21, 1):
-    kpts.append([round(0.005 * i, 3), 0.0, 0.8])
-    kpts.append([round(0.005 * i, 3), 0.0, 1.0])
+a = np.linspace(-0.2, 0.2, 51)
+c = np.linspace(-0.2, 0.2, 51)
+for i in a:
+    for j in c:
+        kpts.append([i, 0.0, j])
 kpts = np.array(kpts)
 np.savetxt('k.dat', kpts, fmt='%.3f')
+
